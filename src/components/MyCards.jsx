4019b4180b0a2e0c14623a413836813b110688cc
GhostGirl03 <152809822+GhostGirl03@users.noreply.github.com>
2024-03-25 23:35:52 +0000

    First commit

--- src/components/MyCards.jsx
+++ src/components/MyCards.jsx
@@ -5,30 +5,30 @@ import Card from 'react-bootstrap/Card';
 
 export function MyCards() {
     return (
-      <Row lg={4}>
-      
-      <Card style={{ width: '18rem' }}>
-    <Card.Body className="flex-column gap-4 ">
-      <col>
+      <Row xs={1} md={2} className="g-4">
+      {Array.from({ length: 4 }).map((_, idx) => (
+    <Card.Body>
+   
+      <Col key={idx}>
       <Card.Title>Card Title</Card.Title>
       <Card.Text>
         Some quick example text to build on the card title and make up the
         bulk of the card's content.
       </Card.Text>
       <Button variant="primary">Find Out More!</Button>
-     </col>
+     </Col>
 
-     <col>
+     <Col>
       <Card.Title>Card Title</Card.Title>
       <Card.Text>
         Some quick example text to build on the card title and make up the
         bulk of the card's content.
       </Card.Text>
       <Button variant="primary">Find Out More!</Button>
-     </col>
+     </Col>
 
     </Card.Body>
-    </Card>
+    ))}
    
     </Row>
 );
